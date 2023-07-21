0ef0a801534c182b3e61b1980c5c6831e5b06466
Wren Wightman <wren.wightman@gmail.com>
2023-07-21 01:11:59 -0400

    Made lagNRho and RofM wrapper functions

--- pyrho/rho/pyrho.py
+++ pyrho/rho/pyrho.py
@@ -1,2 +1,96 @@
+"""Python wrapper for c-type coherence functions"""
 import ctypes as ct
+import cinpy.types as cnpt
+import numpy as np
+from glob import glob
+import platform as _pltfm
+import os
 
+# determine installed path
+dirpath = os.path.dirname(__file__)
+
+# determine the OS and relative binary file
+if _pltfm.uname()[0] == "Windows":
+    res = glob(os.path.abspath(os.path.join(dirpath, "*.dll")))
+    name = res[0]
+elif _pltfm.uname()[0] == "Linux":
+    res = glob(os.path.abspath(os.path.join(dirpath, "*.so")))
+    name = res[0]
+else:
+    res = glob(os.path.abspath(os.path.join(dirpath, "*.dylib")))
+    name = res[0]
+
+# load the c library
+__rho__ = ct.CDLL(name)
+
+__rho__.lagNRho.argtypes = ct.POINTER(ct.POINTER(ct.POINTER(ct.c_float))), ct.c_int, ct.c_int, ct.c_int
+__rho__.lagNRho.restype = ct.c_float
+
+def lagNRho(arr, lag:int=1, axis:int=1):
+    """calculate the Nth lag of 2D input matrix
+    
+    Parameters:
+    ----
+    arr: the array over which the Nth lag coherence is being calculated
+    lag: the lag index
+    axis: the axis corresponding to the elements
+
+    Returns:
+    ----
+    rho: normalized correlation coefficient at this lag
+    """
+
+    # only allow 2D input arrays
+    if np.ndim(arr) != 2:
+        raise ValueError("Input array must be 2D")
+    
+    # Make the sample axis the fast axis
+    if axis == 1:
+        c_arr, M, N = cnpt.copy2c(arr.T)
+    else:
+        c_arr, M, N = cnpt.copy2c(arr)
+
+    # calculate the lag
+    rho = __rho__.lagNRho(ct.byref(c_arr), M, N, ct.c_int(lag))
+    cnpt.free(c_arr, M, N)
+
+    # return the python-friendly value
+    return float(rho)
+
+def RofM(arr, axis:int=1):
+    """calculate the Nth lag of 2D input matrix
+    
+    Parameters:
+    ----
+    arr: the array over which the Nth lag coherence is being calculated
+    lag: the lag index
+    axis: the axis corresponding to the elements
+
+    Returns:
+    ----
+    rho: normalized correlation coefficient at this lag
+    """
+
+    # only allow 2D input arrays
+    if np.ndim(arr) != 2:
+        raise ValueError("Input array must be 2D")
+    
+    # Make the sample axis the fast axis
+    if axis == 1:
+        c_arr, M, N = cnpt.copy2c(arr.T)
+        nele = arr.shape[1]
+    else:
+        c_arr, M, N = cnpt.copy2c(arr)
+        nele = arr.shape[0]
+
+    # make a buffer
+    rhos = np.zeros(nele-1)
+
+    for lag in range(nele-1):
+        # calculate the lag
+        rho = __rho__.lagNRho(ct.byref(c_arr), M, N, ct.c_int(lag))
+        rhos[lag] = float(rho)
+    cnpt.free(c_arr, M, N)
+
+    # return the python-friendly value
+    return rhos
