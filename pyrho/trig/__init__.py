d058b7144f9d81d71bf9cfcce6f71bc3f0d8db95
Wren Wightman <wren.wightman@duke.edu>
2023-08-15 16:58:19 -0400

    Made wrapper function for getting element tabs

---
+++ pyrho/trig/__init__.py
@@ -0,0 +1,1 @@
+from pyrho.trig.pytrig import geteletaus
--- pyrho/trig/pytrig.py
+++ pyrho/trig/pytrig.py
@@ -32,3 +32,34 @@ __trig__.pwtxengine.restype = ct.POINTER(ct.c_float)
 __trig__.genmask3D.argtypes = ct.c_int, ct.c_float, ct.POINTER(ct.POINTER(ct.c_float)), ct.POINTER(ct.POINTER(ct.c_float)), ct.POINTER(ct.POINTER(ct.c_float)), ct.POINTER(ct.POINTER(ct.POINTER(ct.c_float))),
 __trig__.genmask3D.restype = ct.POINTER(ct.c_int)
 
+def geteletaus(eles, points, c:float=1540):
+    """Calculate the time from each point to each element
+    
+    Parameters:
+    ----
+    eles: E by 3 matrix where E is the number of elements
+    points: P by 3 matrix where P is the number of points
+    c: speed of sound, default is 1540 M/s
+
+    Returns:
+    ----
+    Taus: a list of pointers to E arrays, each holing the delay from the respective element to all the points
+    """
+
+    if (np.ndim(eles) != 2): raise ValueError("eles must be 2D")
+    if (eles.shape[1] != 3):raise ValueError("eles must be E by 3")
+    if (np.ndim(points) != 2): raise ValueError("points must be 2D")
+    if (points.shape[1] != 3):raise ValueError("points must be E by 3")
+
+    # extract constants
+    # convert eles and points to c arrays
+    Celes, Me, Ne = cnpt.copy2c(eles)
+    Cpoints, Mp, Np = cnpt.copy2c(points)
+
+    TauEs = []
+    for i in range(Me):
+        ptaue = __trig__.rxengine(Mp, ct.c_float(c), ct.byref(Celes[i]), ct.byref(Cpoints))
+        TauEs.append(ptaue)
+
+    return TauEs
+
